f3a95dd4f04f5e909f03bde1682eae7ae8809ed3
Kristofer Davie <kristofer.davie@gmail.com>
2015-03-05 12:22:00 +0100

    Starting with K&R Chapter 1

---
+++ 001.HelloWorld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	printf("Hello World!\n");
+
+// Testing some excape characters, the terminal doesn't like this too much, beep beep!
+//	printf("Testing some escape characters\n a\a b\b c\c d\d e\e f\f g\g h\h i\i j\j k\k");
+//	printf("l\l m\m n\n o\o p\p q\q r\r s\s t\t v\v w\w y\y z\z 1\1 2\2 3\3 4\4 5\5");
+//	printf("6\6 7\7 8\8 9\9 -\- =\=");
+}
