bc675a5c41a23e1ca9a3bd5652639a04d088e340
Kristofer Davie <kristofer.davie@gmail.com>
2015-03-06 16:54:19 +0100

    Replaced the variables with defines. All in caps, easier to read and change.

--- 002.TempCalc.c
+++ 002.TempCalc.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
+#define MAX 500
+#define MIN -300
+#define STEP 20
 
 int main() {
     float cf, fahr, celsius;
-    float min, max, step;
 
-    min = -300;
-    max = 500;
-    step = 20;
-
-    cf = min;
+    cf = MIN;
     printf("C/F\t|    Fahrenheit\t|   Celsius\n----------------------------------\n");
-    while (cf <= max) {
+    while (cf <= MAX) {
         celsius = (5.0/9.0) * (cf - 32.0);
         fahr = ((cf * (9.0/5.0)) + 32);
         printf("%4.0f\t|\t%4.1f\t|\t%4.1f\n", cf, fahr, celsius);
-        cf = cf + step;
+        cf = cf + STEP;
 
     }
 
