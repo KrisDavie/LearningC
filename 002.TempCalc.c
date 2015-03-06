2e6e1b7ef00331010d5ea4aa35c9faa241d8b505
Kristofer Davie <kristofer.davie@gmail.com>
2015-03-06 16:51:49 +0100

    Basic TempCalc finished.

--- 002.TempCalc.c
+++ 002.TempCalc.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    float fahr, celsius;
+    float cf, fahr, celsius;
     float min, max, step;
 
     min = -300;
     max = 500;
     step = 20;
 
-    fahr = min;
-    printf("Fahrenheit\tCelsius\n");
-    while (fahr <= max) {
-        celsius = (5.0/9.0) * (fahr -   32.0);
-        printf("%4.0f\t|\t%6.1f\n", fahr, celsius);
-        fahr = fahr + step;
+    cf = min;
+    printf("C/F\t|    Fahrenheit\t|   Celsius\n----------------------------------\n");
+    while (cf <= max) {
+        celsius = (5.0/9.0) * (cf - 32.0);
+        fahr = ((cf * (9.0/5.0)) + 32);
+        printf("%4.0f\t|\t%4.1f\t|\t%4.1f\n", cf, fahr, celsius);
+        cf = cf + step;
+
     }
 
 }
