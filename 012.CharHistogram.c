2c6dfee3872a65e817d5c74027bec4b93cb7ec78
Kristofer Davie <kristofer.davie@gmail.com>
2015-03-12 06:07:28 +0100

    Program to print occurances of each ASCII character
    Signed-off-by: Kristofer Davie <kristofer.davie@gmail.com>

---
+++ 012.CharHistogram.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/* A program to print a histogram of character occurances */
+
+int main () {
+    int lengths[128];
+    int c, clen, max;
+
+    max = clen = 0;
+    for (int i = 1; i <= 127; ++i) {
+        lengths[i] = 0;
+    }
+    while ((c = getchar()) != EOF) {
+        ++lengths[c];
+    }
+
+    for (int i = 1; i <= 127; ++i) {
+        if (lengths[i] > max) {
+            max = lengths[i];
+        }
+    }
+
+    for (int i = 1; i <= 127; ++i) {
+        printf("%d\t:", i);
+        for (int l = 1; l <= ((lengths[i]*100)/max); l++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
